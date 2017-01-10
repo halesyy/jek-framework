8e394ce779669de2a9ae235c708287e4a16cb577
Jek Hales <halesyy@gmail.com>
2017-01-09 19:50:19 -0800

    Cuter index.php comments
    The comment graphics look waayyyyy cooler now

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
+  ob_start();
   session_start();
 
-  $_GET['XDEBUG_PROFILE'] = true;
-
   /*
-    Dedicated, passion-driven from the amazing ❤ Kayla ❤ xx
+    Dedicated, passion-driven from the amazing ❤ Kayla ❤ xx [Cool little graphics cause why not go here]
 
-                 ___  _____  _   __
-                |_  ||  ___|| | / /
-                  | || |__  | |/ /
-                  | ||  __| |    \
-              /\__/ /| |___ | |\  \
-              \____/ \____/ \_| \_/
+          .---.
+          |   |                                                         .---.
+          '---'      __.....__          .                               |   |
+          .---.  .-''         '.      .'|                .-.          .-|   |
+          |   | /     .-''"'-.  `.  .'  |                 \ \        / /|   |
+          |   |/     /________\   \<    |            __    \ \      / / |   |    __
+          |   ||                  | |   | ____    .:--.'.   \ \    / /  |   | .:--.'.
+          |   |\    .-------------' |   | \ .'   / |   \ |   \ \  / /   |   |/ |   \ |
+          |   | \    '-.____...---. |   |/  .    `" __ | |    \ `  /    |   |`" __ | |
+          |   |  `.             .'  |    /\  \    .'.''| |     \  /     |   | .'.''| |
+       __.'   '    `''-...... -'    |   |  \  \  / /   | |_    / /      '---'/ /   | |_
+      |      '                      '    \  \  \ \ \._,\ '/|`-' /            \ \._,\ '/
+      |____.'                      '------'  '---'`--'  `"  '..'              `--'  `"
               ______                                                       _
               |  ___|                                                     | |
               | |_    _ __   __ _  _ __ ___    ___ __      __  ___   _ __ | | __
@@ -19,7 +25,6 @@
               | |    | |   | (_| || | | | | ||  __/ \ V  V / | (_) || |   |   <
               \_|    |_|    \__,_||_| |_| |_| \___|  \_/\_/   \___/ |_|   |_|\_\
 
-       - The hackiest framework ever! (Amen to quick development <3)
 
                ::::==========:::::::::::::::::::::::::::::::::::::::::::::::::::::::
                 :::::::=========::::::.---------------.:::::::::::::::::::::::::::::::
@@ -68,6 +73,11 @@
 
                         佛祖保佑         永无BUG
 
+              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+              Papa bless your code, let it be bug-free.
+
+              ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   */
 
 
@@ -78,6 +88,9 @@
   | ------------------------------------------------------------------------------------
   | QUICK RUN THROUGH ON CONSEPTS OF THIS FRAMEWORK.
   | ------------------------------------------------------------------------------------
+  | @NOTE Since this was written, lots of the framework has changed, so please wait
+  | around for new documentation.
+  |
   | A normal (most) framework runs off MVC (Model -> View -> Controller), which is the
   | core of this framework, but the names have been re-written, so:
   | MVC = JEK
@@ -88,7 +101,7 @@
   | When a browser connects to the framework, we run a Routing system,
   | so if the browser calls "localhost/name/jack", in your "sys/build/Routing.php"
   | file, you can specify what Kontroller that calls.
-  | NOTE Read more in the file itself for more information on how to Route.
+  | @NOTE Read more in the file itself for more information on how to Route.
   |
   | In a Kontroller, the main function is to process data, such as call a Joint class
   | to get some data from a Database, which you can specify Database connection vals from
@@ -100,18 +113,17 @@
 
   //************************************************************************************
 
-  //Each page load loads the Init file.
+  /*
+  | ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  */
 
 
+    require_once "sys/Init.php";
 
+  /*
+  | ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  */
 
+  //***********************************************************************************
 
-
-  require_once "sys/Init.php";
-
-
-
-
-
-
-  //
+  ob_flush();
